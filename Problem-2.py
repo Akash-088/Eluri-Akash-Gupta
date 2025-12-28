c6109beedb6b72ed1e5be899f97e22585573d3fc
Akash-088 <eluri.akash2310@gmail.com>
2025-12-28 12:50:45 +0530

    added problem 2

---
+++ Problem-2.py
@@ -0,0 +1,18 @@
+a=int(input("Enter a number: "))
+res=[]
+
+for i in range(a):
+    res.append(2*i+1)
+
+print(",".join(map(str,res)))
+
+
+# a=4
+# res=[]
+
+# for i in range(a):
+#     res.append(2*i+1)
+
+# print(",".join(map(str,res)))
+
+
