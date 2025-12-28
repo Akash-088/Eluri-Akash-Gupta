068e312de9308a628783f48dc87ce12920a1b9f5
Akash-088 <eluri.akash2310@gmail.com>
2025-12-28 12:42:29 +0530

    Update

--- Problem-1.py
+++ Problem-1.py
@@ -25,3 +25,31 @@ operation=input("Enter operation(+,-,*,/): ")
 
 calc=calculator(a,b,operation)
 print(calc.calculate())
+
+
+
+
+# class Calculator:
+#     def __init__(self,a:float,b:float,operation:str):
+#         self.a=a
+#         self.b=b
+#         self.operation=operation
+
+#     def calculate(self):
+#         if self.operation=="+":
+#             return self.a+self.b
+#         elif self.operation=="-":
+#             return self.a-self.b
+#         elif self.operation=="*":
+#             return self.a*self.b
+#         elif self.operation=="/":
+#             if self.b!=0:
+#                 return self.a/self.b
+#             else:
+#                 return "Division by zero is not possible"
+#         else:
+#             return "Invalid operation"
+
+
+# calc = Calculator(10.5,2.5,"+")
+# print(calc.calculate())
