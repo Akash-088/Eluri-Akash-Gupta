45d6935da4bc1e3a5e142d8fbcaf1e8942a32257
Akash-088 <eluri.akash2310@gmail.com>
2025-12-28 13:08:38 +0530

    added problem 3

--- Problem-3.py
+++ Problem-3.py
@@ -1,20 +1,20 @@
-# a=int(input("Enter a number: "))
-# count=a if a%2!=0 else a-1
-
-# res=[]
-# for i in range(count):
-#     res.append(2*i+1)
-
-# print(", ".join(map(str,res)))
-
-
-a=3
+a=int(input("Enter a number: "))
 count=a if a%2!=0 else a-1
+
 res=[]
-for i in range(a):
+for i in range(count):
     res.append(2*i+1)
 
 print(", ".join(map(str,res)))
+
+
+# a=3
+# count=a if a%2!=0 else a-1
+# res=[]
+# for i in range(a):
+#     res.append(2*i+1)
+
+# print(", ".join(map(str,res)))
 
 # a=4
 # count=a if a%2!=0 else a-1
